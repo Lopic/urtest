1c5bbfa6dda331061a8f637fe76389a462fdb8f8
xicmiah <xicmiah@gmail.com>
2009-11-21 22:41:03 +0300

    Добавляю содержимое templates

--- bugtracker/views.py
+++ bugtracker/views.py
@@ -19,3 +19,43 @@ def add_bug(request):
 		form = BugForm()
 	return render_to_response('addbug.html',{'form': form})
 
+
+
+       # авторизация
+def login(request):
+    return render_to_response('login.html')
+
+def logout(request):
+    return render_to_response('logout.html')
+
+       # компании
+def company_registraion(request):
+    return render_to_response('company_registraion.html')
+
+def companies_list(request):
+    return render_to_response('companies_list.html', {'pk': 12})
+
+def company_details(request,project):
+    return render_to_response('company_details.html', {'pk': 13})
+
+       #тестеры
+def tester_registraion(request):
+    return render_to_response('tester_registraion.html')
+
+def testers_list(request):
+    return render_to_response('testers_list.html', {'pk': 12})
+
+def tester_details(request,project):
+    return render_to_response('tester_details.html', {'pk': 22})
+
+      # Пректы
+def new_project(request):
+    return render_to_response('new_project.html')
+
+def projects_list(request):
+    return render_to_response('projects_list.html', {'pk': 12})
+
+def project_details(request,project):
+    return render_to_response('project_details.html')
+
+
--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 # File encoding: utf-8
+
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
@@ -10,14 +11,45 @@ from django.views.generic.simple import direct_to_template
 # Родные для сайта виды
 # Импорт делается автоматически
 urlpatterns = patterns('urtest.bugtracker.views',
-	(r'^$', direct_to_template, {'template': 'base.html'}),
-    (r'^bugs/$', 'bugs_list'),
-    (r'^bugs/add$', 'add_bug'),
+    # Главная страница
+    (r'^$', direct_to_template, {'template': 'base.html'}),
+    # Авторизация
+    (r'^login$', 'login'),
+    # Выход
+    (r'^logout$', 'logout'),
+
+    # Старые примеры работы с багами
+    #(r'^bugs/$', 'bugs_list'),
+    #(r'^bugs/add$', 'add_bug'),
+
+    # Страницы для тестеров:
+    # Список всех тестеров
+    (r'^testers/$', 'testers_list'),
+    # Личная страница тестера
+    (r'^testers/(\d+)$', 'tester_details'),
+    # Регистрация нового тестера
+    (r'^testers/register$', 'tester_registraion'),
+
+    # Страницы компаний:
+    # Список всех компаний
+    (r'^companies/$', 'companies_list'),
+    # Личная страница компании
+    (r'^companies/(\d+)$', 'company_details'),
+    # Регистрация новой компании
+    (r'^companies/register$', 'company_registraion'),
+
+    # Страницы проектов:
+    # Список всех проектов
+    (r'^projects/$', 'projects_list'),
+    # Страница проекта
+    (r'^projects/(\d+)$', 'project_details'),
+    # Добавление проекта
+    (r'^projects/new$', 'new_project'),
 
     # Example:
     # (r'^urtest/', include('urtest.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
@@ -27,6 +59,7 @@ urlpatterns = patterns('urtest.bugtracker.views',
 
 # Статические файлы: CSS и тд
 urlpatterns += patterns('',
-	(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': 'media'}),
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+       {'document_root': 'media'}),
+
 )
